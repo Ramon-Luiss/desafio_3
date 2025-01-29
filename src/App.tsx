ff723632e9e4c9ebf7371b440c33c96f9fd1b4db
Ramonstro <ramon.ln21999@gmail.com>
2025-01-29 03:25:36 -0300

    Adicionando predeploy e deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import SignIn from "./pages/SignIn.tsx";
 import Search from "./pages/Search.tsx";
